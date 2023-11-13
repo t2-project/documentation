9e25258db57fadff544ece756fa6e9e1b134ab95
David Kopp <git@davidkopp.de>
2023-11-13 11:56:54 +0100

    Change font to Arial

--- source/conf.py
+++ source/conf.py
@@ -79,7 +79,10 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'font_family': '"Arial", Helvetica',
+    'head_font_family': '"Arial", Helvetica',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
